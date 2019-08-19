12873e62eadd898098bc8fde48643a1c977a76f6
Xiao Gao <gaoxiao1988@gmail.com>
2019-08-19 00:39:03 -0700

    Add waveglow formatter.

---
+++ tools/convert_waveglow_format.py
@@ -0,0 +1,44 @@
+import os
+from random import shuffle
+
+
+def process_text(text):
+    return text.replace('--s', '').strip()
+
+
+def main():
+    orig_path = '/home/xiao/code/ai_utils/tts_data/downsampled_Sam_transcripts.txt'
+    if not os.path.isfile(orig_path):
+        print('File not found: {}, skipping'.format(orig_path))
+        return
+
+    to_calculate = []
+    with open(orig_path) as f:
+        for l in f:
+            try:
+                text, path = l.split('|')
+                text = process_text(text)
+                path = '/home/xiao/code/ai_utils/tts_data/{}'.format(path.strip())
+                if len(text.split()) > 20:
+                    continue
+                to_calculate.append(path)
+            except Exception:
+                print('Error format: {}'.format(l))
+
+    size = len(to_calculate)
+    shuffle(to_calculate)
+    train_size = int(size * 0.99)
+    train_set = to_calculate[:train_size]
+    test_set = to_calculate[train_size:]
+
+    def write_file(line_set, file_path):
+        with open(file_path, 'w') as f:
+            for l in line_set:
+                f.write('{}\n'.format(l))
+
+    write_file(train_set, 'train_waveglow.txt')
+    write_file(test_set, 'test_waveglow.txt')
+
+
+if __name__ == '__main__':
+    main()
