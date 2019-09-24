d257dd693b862f61a98e712bab2be0b8ca387e72
Xiao Gao <gaoxiao1988@gmail.com>
2019-09-23 18:00:39 -0700

    Add infer cn.

---
+++ infer_cn.py
@@ -0,0 +1,80 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import sys
+import time
+
+import matplotlib.pylab as plt
+import scipy
+
+sys.path.append('waveglow/')
+import numpy as np
+import torch
+
+from hparams import create_hparams
+from train import load_model
+from text import text_to_sequence
+
+
+def plot_data(data, figsize=(16, 4)):
+    fig, axes = plt.subplots(1, len(data), figsize=figsize)
+    for i in range(len(data)):
+        axes[i].imshow(data[i], aspect='auto', origin='bottom',
+                       interpolation='none')
+
+
+hparams = create_hparams()
+# hparams.sampling_rate = 22050
+
+checkpoint_path = "outdir/checkpoint_1000"
+# checkpoint_path = "tacotron2_statedict.pt"
+
+model = load_model(hparams)
+model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+_ = model.cuda().eval().half()
+
+waveglow_path = 'waveglow_256channels.pt'
+#waveglow_path = 'waveglow/checkpoints1/saved_214000'
+waveglow = torch.load(waveglow_path)['model']
+waveglow.cuda().eval().half()
+for k in waveglow.convinv:
+    k.float()
+
+# text = "Read loudly, and be a super hero!"
+# sequence = np.array(text_to_sequence(text, ['english_cleaners']))[None, :]
+# sequence = torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
+
+text_list = [
+    # 'lion.',
+    # "Join me to learn some words.",
+    # "Tom likes drums",
+    # "Tom does not like kites",
+    # "Now you try!",
+    # "Wow, you matched all of the words!",
+    # "Great work reading those words!",
+    # "It's time for some vocabulary!",
+    # "Hi, Andrew here!",
+    # "Let's review what you said",
+    # "baseball",
+    # "Sorry. I did not hear you. Could you say it louder?",
+    # "Hello, Red Beetle",
+    # "HIllary and Henry are hiding in their school. Can you find them?",
+    'yin1 er2 ta1 de5 yan3 zou4 zhi4 pu3 er2 sa3 tuo1 yin1 se4 ji2 yun4 lv4 ru2 xing2 yun2 liu2 shui3 ban1 shu1 chang4 er2 chun2 mei3'
+]
+for text in text_list:
+    start_time = time.time()
+    sequence = np.array(text_to_sequence(text, ['english_cleaners']))[None, :]
+    print("--- text to seq: %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+
+    sequence = torch.autograd.Variable(torch.from_numpy(sequence)).cuda().long()
+    mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+    print("--- tacotron2 %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+
+    with torch.no_grad():
+        audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+    print("--- waveglow %s seconds ---" % (time.time() - start_time))
+
+    data = audio[0].data.cpu().numpy().astype(np.float32)
+    scipy.io.wavfile.write('audio_output/{}.wav'.format(text), hparams.sampling_rate, data)
